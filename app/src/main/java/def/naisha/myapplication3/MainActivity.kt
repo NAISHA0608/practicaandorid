477c681b8d8e064cdbc772ef4e4ab9b8c2a13993
NAISHA0608 <naishaayovi00@gmail.com>
2024-12-16 17:56:00 -0500

    Practica

--- app/src/main/java/def/naisha/myapplication3/MainActivity.kt
+++ app/src/main/java/def/naisha/myapplication3/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp // Import necesario para dp
-import def.naisha.myapplication3.ui.theme.MyApplication3Theme
+import def.naisha.myapplication3.layout.Saludo
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,24 +17,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Saludo() {
-    Column(
-        modifier = Modifier.padding(vertical = 50.dp, horizontal = 16.dp)
-    ) {
-        Text(
-            text = "Hello"
-        )
-        Text(
-            text = "Mi nombre es Naisha Ayovi"
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewSaludo() {
-    MyApplication3Theme {
-        Saludo()
-    }
-}
---
+++ app/src/main/java/def/naisha/myapplication3/layout/ColumnsLayout.kt
@@ -0,0 +1,41 @@
+package def.naisha.myapplication3.layout
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp // Import necesario para dp
+import def.naisha.myapplication3.ui.theme.MyApplication3Theme
+
+@Composable
+fun Saludo() {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(vertical = 36.dp, horizontal = 8.dp)
+            .background(color = androidx.compose.ui.graphics.Color.Red)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Hello"
+        )
+        Text(
+            text = "Mi nombre es Mario Mendez"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSaludo() {
+    MyApplication3Theme {
+        Saludo()
+    }
+}
